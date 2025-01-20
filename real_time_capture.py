9574e38e97cc75430730df28bdfa73b1bb4e1d12
TrongMinh <trongminhmcw10e@gmail.com>
2025-01-20 15:13:30 +0700

    i failed

--- real_time_capture.py
+++ real_time_capture.py
@@ -4,6 +4,28 @@ import time
 
 cam = cv2.VideoCapture("/home/tminh/Downloads/jmdva.mp4")
 
+# Get video frame dimensions
+frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+# Define rectangle size
+rect_width = 100
+rect_height = 25
+
+# Region of interest center
+roi_center_x = rect_width // 2
+roi_center_y = rect_height // 2
+
+# Calculate the center position
+center_x = frame_width // 2
+center_y = frame_height // 2
+
+# Define the top-left and bottom-right corners of the rectangle
+x1 = center_x - rect_width // 2
+y1 = center_y - rect_height // 2
+x2 = x1 + rect_width
+y2 = y1 + rect_height
+
 lower_purple = np.array([120, 50, 50])  # Lower boundary of purple
 upper_purple = np.array([160, 255, 255])  # Upper boundary of purple
 
@@ -18,13 +40,23 @@ while cam.isOpened():
         break
     
     #region process frame
+    roi = image[y1:y2, x1:x2] #region of interests
+
     # Convert the image to HSV color space
-    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    hsv_image = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
 
     purple_mask = cv2.inRange(hsv_image, lower_purple, upper_purple)
     binary_mask = (purple_mask > 0).astype(np.uint8)
-    # Apply the mask to the original image
-    # purple_highlight = cv2.bitwise_and(image, image, mask=purple_mask)
+
+    if binary_mask[roi_center_y,roi_center_x] == 1:
+        print("shoot, middle is purple")
+    else:
+        left_side = binary_mask[roi_center_y, :roi_center_x]
+        right_side = binary_mask[roi_center_y, roi_center_x+1:]
+        
+        if 1 in left_side and 1 in right_side:
+            print("shoot, left and right are purple")
+
     #endregion
     
     #region calculate fps
@@ -34,8 +66,9 @@ while cam.isOpened():
     prev_frame_time = new_frame_time
 
     fps = str(int(fps))
+
     # put fps on frame
-    cv2.putText(binary_mask, fps, (7, 70), font, 3, (100, 255, 0), 3, cv2.LINE_AA) 
+    cv2.putText(binary_mask, fps, (0, 20), font, 1, (100, 255, 0), 3, cv2.LINE_AA) 
     #endregion
 
     #region display
